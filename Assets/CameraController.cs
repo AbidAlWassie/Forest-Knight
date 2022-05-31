1d78e1d483f487db3a5be273dc402c516ff80cfe
Abid Al Wassie <abidalwassie@outlook.com>
2022-06-01 00:28:11 +0600

    fixed camera to player movement

---
+++ Assets/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10);
+    }
+}
