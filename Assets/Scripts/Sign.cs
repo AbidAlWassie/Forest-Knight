72ab98f8dcbb0d3b86bd672a5468f50e97259f54
Abid Al Wassie <abidalwassie@outlook.com>
2022-07-28 22:39:48 +0600

    *
    added sign interaction

---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")) 
+        {
+            playerInRange = true;
+            Debug.Log("Player in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+            Debug.Log("Player left range");
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            if (dialogBox.activeInHierarchy) {
+                dialogBox.SetActive(false);
+            } else
+            {
+                dialogBox.SetActive(true);
+            }
+        }
+    }
+
+    
+}
